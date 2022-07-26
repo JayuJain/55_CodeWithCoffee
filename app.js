2fbb8de826e3399473fbe83ddc90b7b30fd9b549
Archit <architrathod77@gmail.com>
2022-07-26 13:29:34 +0530

    uri info removed

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// const bodyParser = require('body-parser');
-// const express = require('express');
-
-
-// const mongoose = require('mongoose');
-// // mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true });
-
-// const mongoAtlasUri =
-//     "mongodb+srv://Test1:Test1@cluster0.ua0e3.mongodb.net/?retryWrites=true&w=majority";
-
-// try {
-//     // Connect to the MongoDB cluster
-//     mongoose.connect(
-//         mongoAtlasUri,
-//         { useNewUrlParser: true, useUnifiedTopology: true },
-//         () => console.log(" Mongoose is connected"),
-//     );
-// } catch (e) {
-//     console.log("could not connect");
-// }
-
-// const dbConnection = mongoose.connection;
-// dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
-// dbConnection.once("open", () => console.log("Connected to DB!"));
-
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
